58b196ed589a6550634f71af74222bca00df9767
yashika29092006 <yashikaa2529@gmail.com>
2025-12-31 22:29:43 +0530

    Fixing internal server error

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,17 +3,13 @@ from app.database.db import Base, engine
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers import user, admin, station, review, contact, super_admin
 
-# Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="CNGate Backend")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://cngate-frontend.netlify.app",
-        "http://localhost:3000",
-        "http://127.0.0.1:3000"
-    ], 
+    allow_origins=["https://cngate-frontend.netlify.app", "http://localhost:3000"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
