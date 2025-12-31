964de488e9452c9d734d56d22d16841d67d25e79
yashika29092006 <yashikaa2529@gmail.com>
2025-12-31 22:23:44 +0530

    Fixing error on CORS middleware for netlify

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,11 @@ app = FastAPI(title="CNGate Backend")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://cngate-frontend.netlify.app"], 
+    allow_origins=[
+        "https://cngate-frontend.netlify.app",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000"
+    ], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
