bd8665a26671ea527b0b5578c39b43bada8f1887
yashika29092006 <yashikaa2529@gmail.com>
2026-03-10 17:13:29 +0530

    testing

--- js/user-map.js
+++ js/user-map.js
@@ -8,7 +8,7 @@ function initMap() {
     if (map) return;
 
     // Default center (can be adjusted)
-    c = map = L.map('map').setView([13.0827, 80.2707], 12); // Chennai coordinates as default
+    map = L.map('map').setView([13.0827, 80.2707], 12); // Chennai coordinates as default
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; OpenStreetMap contributors'
@@ -18,7 +18,7 @@ function initMap() {
     setTimeout(() => {
         map.invalidateSize();
     }, 1000);
-    console.log(c)
+    console.log(z,x,y,s)
 }
 
 export function addStationMarkers() {
